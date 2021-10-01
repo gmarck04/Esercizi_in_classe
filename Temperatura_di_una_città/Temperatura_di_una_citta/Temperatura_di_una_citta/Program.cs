13c32ead5805dbc5e0ca68d70455ac454e633b08
Giovanni <gmarck04@gmail.com>
2021-10-01 09:22:59 +0200

    Esercizio 4

---
+++ Temperatura_di_una_città/Temperatura_di_una_citta/Temperatura_di_una_citta/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Dopo aver acquisito da tastiera una serie di dati relativi alla misurazione della temperatura di una
+ *certa città, scrivere il codice di un programma(OOP) in C# che determini e visualizzi la temperatura
+ *più bassa e quella più alta. */
+
+using System;
+
+namespace Temperatura_di_una_citta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
