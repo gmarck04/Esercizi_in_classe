86ca20bb375ca88143efadcb93d199afef3c3713
Giovanni <gmarck04@gmail.com>
2021-10-08 09:10:29 +0200

    Aggiunta dei commenti

--- Negozio_smartphone/Negozio_smartphone/Negozio_smartphone/Program.cs
+++ Negozio_smartphone/Negozio_smartphone/Negozio_smartphone/Program.cs
@@ -1,31 +1,35 @@
-﻿/*Dopo aver acquisito da tastiera una serie di prezzi relativi ai cellulari in vendita in un negozio,
+﻿/* Nickname: @gmarck04
+ * Data: 07/10/2021
+ * Consegna: Dopo aver acquisito da tastiera una serie di prezzi relativi ai cellulari in vendita in un negozio,
  * scrivere il codice di un programma(OOP) in C# che visualizzi i prezzi maggiori di 100€. */
 
 using System;
 
 namespace Negozio_smartphone
 {
-    class telefono
+    class telefono //Classe che mi registra tutti i telefoni e i loro prezzi e mi restituisce i telefoni con prezzo maggiore di 100€.
     {
-        string[] nome_telefono = new string[0];
-        float[] prezzo_telefono = new float[0];
+        //Attributi
+        protected string[] nome_telefono = new string[0];
+        protected float[] prezzo_telefono = new float[0];
 
-        string[] nome_telefono_alto = new string[0];
-        float[] prezzo_telefono_alto = new float[0];
+        public string[] nome_telefono_alto = new string[0];
+        public float[] prezzo_telefono_alto = new float[0];
 
-        //metodi
-        public void Inserisci_nome(string nome)
+        //Metodi
+        public void Inserisci_nome(string nome) //Metodo che inserisce i nomi nell'array nome_telefono
         {
             Array.Resize(ref nome_telefono, nome_telefono.Length + 1);
             nome_telefono[nome_telefono.Length - 1] = nome;
         }
-        public void Inserisci_prezzo(float prezzo)
+        public void Inserisci_prezzo(float prezzo) //Metodo che inserisce i prezzi nell'array prezzi_telefono
         {
             Array.Resize(ref prezzo_telefono, prezzo_telefono.Length + 1);
             prezzo_telefono[prezzo_telefono.Length - 1] = prezzo;
             Guarda_prezzo();
         }
-        public void Guarda_prezzo()
+        protected void Guarda_prezzo() /*Metodo che guarda i telefoni con prezzo superiore a 100€ e inserisce i prezzi e in nomi 
+                                        * negli array prezzi_telefono_alto e nome_telefono_alto. */
         {
             if (prezzo_telefono[prezzo_telefono.Length - 1] > 100)
             {
@@ -36,20 +40,11 @@ namespace Negozio_smartphone
                 nome_telefono_alto[nome_telefono_alto.Length - 1] = nome_telefono[nome_telefono.Length - 1];
             }
         }
-        public void ToString()
-        {
-            int cont = 0;
-            for (int i = 0; i < prezzo_telefono_alto.Length; i++)
-            {                
-                Console.WriteLine("Nome: {0}", nome_telefono_alto[cont]);
-                Console.WriteLine("Prezzo {0}", prezzo_telefono_alto[cont]);
-                cont++;
-            }
-        }
     }
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) /* Mi richiede il prezzo e il nome del telefono, li invia alla classe telefono, che me li restituirà, 
+                                         * se maggiori di 100€ e che scriverò con la Console. */
         {
             telefono c = new telefono();
             int valore = 1;        
@@ -57,24 +52,32 @@ namespace Negozio_smartphone
             do
             {
                 Console.WriteLine("Inserisci nome telefono");
-                c.Inserisci_nome(Console.ReadLine());
+                c.Inserisci_nome(Console.ReadLine()); //Invio nome telefono.
 
                 Console.WriteLine("Inserisci prezzo telefono");
                 c.Inserisci_prezzo(float.Parse(Console.ReadLine()));
                 Console.WriteLine("Vuoi continuare? Inserisci 1 per stoppare e 0 per continuare");
-                valore = Int32.Parse(Console.ReadLine());
+                valore = Int32.Parse(Console.ReadLine()); //Invio prezzo telefono.
                 while (valore != 0 && valore != 1)
                 {
                     Console.WriteLine("Inserisci nome telefono");
-                    c.Inserisci_nome(Console.ReadLine());
+                    c.Inserisci_nome(Console.ReadLine()); //Invio nome telefono.
 
                     Console.WriteLine("Inserisci prezzo telefono");
-                    c.Inserisci_prezzo(float.Parse(Console.ReadLine()));
+                    c.Inserisci_prezzo(float.Parse(Console.ReadLine())); //Invio prezzo telefono.
                 }
             } while (valore == 0);
 
             Console.Clear();
-            c.ToString();
+
+            //Stampa a schermo i valori inseriti negli array nome_telefono_alto e prezzo_telefono_alto della classe telefono.
+            int cont = 0;
+            for (int i = 0; i < c.prezzo_telefono_alto.Length; i++)
+            {
+                Console.WriteLine("Nome: {0}", c.nome_telefono_alto[cont]);
+                Console.WriteLine("Prezzo {0}", c.prezzo_telefono_alto[cont]);
+                cont++;
+            }
         }
     }
 }
